0850dc114b98352313cf312aa2390619d74eed68
Nathan Kennedy <nathankennedy@Nathans-MacBook-Pro.local>
2019-12-09 15:53:24 +1300

    added additional tests

--- test/runner.js
+++ test/runner.js
@@ -10,7 +10,7 @@ let options = {
     cwd: process.cwd(),
 
     dist: {
-        src: "./",
+        src: "/",
         dest: dest,
         baseurl: "baseurl"
     },
@@ -23,24 +23,37 @@ let options = {
 options.dist.fullPathToSource = path.resolve(options.cwd, options.dist.src);
 options.dist.fullPathToDest = path.resolve(options.cwd, options.dist.dest, options.dist.baseurl);
 
-describe ("clean", function() {
+describe ("clean", async function() {
     before(function () {
         fs.mkdirSync(dest);
     })
 
-    context ("Removing a file", function(){
-        it("should remove the file", async function () {
+    await context ("Removing a file", function(){
+        it("should remove the directory", async function () {
+            options.dist.dest = dest;
             let res = await runner.clean( options )
             console.log(res);
             expect(res).to.eql([path.resolve(options.dist.dest)]);
         })
     })
 
-
+    
+    await context ("invalid directory name", function(){
+        options.dist.dest = "thisdoesntexist"
+        it("should return an empty array", async function () {
+            let res = await runner.clean( options )
+            expect(res).to.eql([]);
+        })
+    })
 
     
     after(function () {
         fs.rmdirSync("test/testdir", {recursive: true})
     })
+    
+})
+
+
+describe ("serve", async function() {
     
 })
