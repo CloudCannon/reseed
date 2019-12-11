1bfa39d9a4863c563d090fc88300cf57c4a7f8e3
Nathan Kennedy <nathankennedy@Nathans-MacBook-Pro.local>
2019-12-12 11:23:04 +1300

    Refactored for easier testing

--- cli.js
+++ cli.js
@@ -4,26 +4,6 @@ const path = require("path");
 const log = require('fancy-log');
 const chalk = require('chalk');
 
-const helpString = `
-Usage: dist <command> <flags>
-Flags:
-    -s | --source   The source folder to clone. Defaults to dist/site.
-    -d | --dest     The destination folder to clone the files to. Defaults to dist/prod
-    -b | --baseurl  The filename to prepend to the files in the source.
-    -p | --port     The portnumber to serve the cloned site on.
-
-Commands:
-    --Command--                                                     --Reqd flags--
-    build           Compiles HTML and CSS to be run at a baseurl.      --baseurl
-    clean           Removes all files from the dest folder.            --dest
-    clone-assets    Clones non CSS and HTML files from src to dest.    --baseurl
-    rewrite-css     Clones CSS files from src to dest and rewrites     
-                    urls to include baseurl.                           --baseurl
-    rewrite-html    Clones HTML files from src to dest and rewrites
-                    attributes to include baseurl.                     --baseurl
-    serve           Runs a local webserver on the dest folder.
-    watch           Watches the src folder and triggers builds.
-`
 
 
 const command = ( func, requiredFlags = []) => {
@@ -50,28 +30,6 @@ const defaultSrc = "dist/site"
 const defaultDest = "dist/prod";
 const defaultPort = 9000;
 
-const cli = meow(
-    helpString, 
-    {
-    flags: {
-        source: { 
-            type: 'string',
-            alias: 's'
-        },
-        dest: {
-            type: 'string',
-            alias: 'd'
-        },
-        baseurl: {
-            type: 'string',
-            alias: 'b'
-        },
-        port: {
-            type: 'string',
-            alias: 'p'
-        }
-    }
-});
 
 
 module.exports = { 
@@ -138,7 +96,7 @@ module.exports = {
         return options;
     },
 
-    run: async function () {
+    run: async function ( cli ) {
 
         let options = this.setOptions( cli.flags );
 
@@ -148,11 +106,9 @@ module.exports = {
         let cmd = cli.input[0] || "dist";
         
         if (commands[cmd]){
-            if (!this.checkRequiredFlags(cli.flags, commands[cmd].requiredFlags)){
-                process.exit(1);
-            }
-            await commands[cmd].run.call(runner, options); //run function in the context of the runner module.
-            
+            if (this.checkRequiredFlags(cli.flags, commands[cmd].requiredFlags)){
+                await commands[cmd].run.call(runner, options); //run function in the context of the runner module.
+            } 
         } else {
             log(chalk.red("command not recognized"));
         }
--- index.js
+++ index.js
@@ -1,4 +1,50 @@
 #!/usr/bin/env node
 const cli = require('./cli');
+const meow = require('meow');
+const helpString = `
+Usage: dist <command> <flags>
+Flags:
+    -s | --source   The source folder to clone. Defaults to dist/site.
+    -d | --dest     The destination folder to clone the files to. Defaults to dist/prod
+    -b | --baseurl  The filename to prepend to the files in the source.
+    -p | --port     The portnumber to serve the cloned site on.
 
-cli.run();
+Commands:
+    --Command--                                                     --Reqd flags--
+    build           Compiles HTML and CSS to be run at a baseurl.      --baseurl
+    clean           Removes all files from the dest folder.            --dest
+    clone-assets    Clones non CSS and HTML files from src to dest.    --baseurl
+    rewrite-css     Clones CSS files from src to dest and rewrites     
+                    urls to include baseurl.                           --baseurl
+    rewrite-html    Clones HTML files from src to dest and rewrites
+                    attributes to include baseurl.                     --baseurl
+    serve           Runs a local webserver on the dest folder.
+    watch           Watches the src folder and triggers builds.
+`
+
+
+const inputs = meow(
+    helpString, 
+    {
+    flags: {
+        source: { 
+            type: 'string',
+            alias: 's'
+        },
+        dest: {
+            type: 'string',
+            alias: 'd'
+        },
+        baseurl: {
+            type: 'string',
+            alias: 'b'
+        },
+        port: {
+            type: 'string',
+            alias: 'p'
+        }
+    }
+});
+
+
+cli.run( inputs );
